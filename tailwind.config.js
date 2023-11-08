1f3bec9755d48e913be5a35bbfe35fcf7c572791
abroudoux <arthur.broudoux@gmail.com>
2023-11-08 14:44:01 +0100

    tailwind custom classes & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
-    content: [
-        './src/**/*.{js,jsx,ts,tsx}',
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
-
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    purple: "var(--purple)",
+    "purple-light": "var(--purple-light)",
+    black: "var(--black)",
+    "black-purple": "var(--black-purple)",
+    white: "var(--white)",
+    "grey-light": "var(--grey-light)",
+    "grey-light-lighten": "var(--grey-light-lighten)",
+    "grey-dark": "var(--grey-dark)",
+    "grey-dark-lighten": "var(--grey-dark-lighten)",
+    "purple-borders": "var(--purple-borders)",
+    "purple-interractive": "var(--purple-interractive)",
+  },
+};
